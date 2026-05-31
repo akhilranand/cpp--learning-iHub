2197520c8de3f658cf8d7abc5f3b329ab6d4efd0
akhilranand <197573077+akhilranand@users.noreply.github.com>
2026-05-31 20:54:56 +0530

    password generator

--- day-5/04_randonPasswordGenerator.cpp
+++ day-5/04_randonPasswordGenerator.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
+#include<ctime>
 
-int main() {
 
+int main(){
     srand(time(0));
+    int len;
+    std::string password;
+    std::cout << "Enter the number of digits needed for your password : ";
+    std::cin>>len;
+    std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*?.,1234567890";
 
-    std::string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    std::string lower = "abcdefghijklmnopqrstuvwxyz";
-    std::string numbers = "0123456789";
-    std::string special = "!@#$%^&*";
+    //std::cout << chars.length() << std::endl;
 
-    std::string all = upper + lower + numbers + special;
-
-    int passwordLength = 10;
-
-    std::string password = "";
-
-    for(int i = 0; i < passwordLength; i++) {
-
-        int randomIndex = rand() % all.length();
-
-        password += all[randomIndex];
+    for(int i =1 ; i<= len;i++){
+        
+        password += chars[rand() % chars.length()] ;
+       
     }
 
-    std::cout << "Generated Password: " << password;
+    std::cout << "password : "<< password <<std::endl;
+    std::cout << std::endl;
+
+   
 
     return 0;
 }
